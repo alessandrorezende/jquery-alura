0584c7db664f336de8e8200276c7fca6f342b40c
Alessandro Rezende <alessandropereirarezende@gmail.com>
2019-06-29 19:23:34 -0300

    Functions and Events in JQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,63 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+//inicializa documento
+var tempoMax = 10;
+var tempoInit = 0;
 var campo = $(".campo-digitacao");
-campo.on("click",function(){
-  console.log(campo.val()); //mostrar no console o conteudo do campo-digitacao
+
+$(document).ready(function(){
+  inicializaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  reiniciarJogo();
+  $("#botao-reiniciar").click(reiniciarJogo);
 });
+
+//------------------------------------------------------------------------------
+//1- parte
+function inicializaTamanhoFrase(){
+  var frase = $(".frase").text();
+  var numPalavras = frase.split(" ").length;
+  $("#frase-size").text(numPalavras);
+  $("#caracteres-size").text(frase.length);
+}
+//------------------------------------------------------------------------------
+//2- parte
+function inicializaContadores(){
+  var qtdFrase = $("#qtd-frase");
+
+  campo.on("input",function(){
+    var conteudo = campo.val();
+    //console.log(campo.val()); //mostrar no console o conteudo do campo-digitacao
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    qtdFrase.text(qtdPalavras);
+
+    //Retira os espaço da String
+    var conteudoSemEspaco = conteudo.replace(/\S+/,'');
+    $("#qtd-caracteres").text(conteudoSemEspaco.length);
+  });
+}
+//------------------------------------------------------------------------------
+//3- parte
+function inicializaCronometro(){
+  var tempo = tempoMax;
+  campo.one("focus",function(){
+    var cronometroID = setInterval(function(){
+      tempo--;
+      //console.log(tempo);
+      $("#tempo-segundos").text(tempo);
+      if(tempo < 1){
+        campo.attr("disabled",true);
+        clearInterval(cronometroID);
+      }
+    },1000);
+  });
+}
+//------------------------------------------------------------------------------
+//4- parte
+function reiniciarJogo(){
+    campo.attr("disabled",false);
+    campo.val("");
+    $("#qtd-caracteres").text(tempoInit);
+    $("#qtd-frase").text(tempoInit);
+    $("#tempo-segundos").text(tempoMax);
+    inicializaCronometro();
+}
